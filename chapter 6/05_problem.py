a3269d1eacd0ce2d1ef9ea4c22e93a86e6a5c766
Rayhan Islam Prome <rayhanspydet@gmail.com>
2026-06-20 21:22:24 +0600

    Add files via upload

---
+++ chapter 6/05_problem.py
@@ -0,0 +1,9 @@
+l =["Rayhan", "Aahil", "Yahaan", "Rifat", "Mustafiz", "Moneem"]
+
+name =input("Enter your name:")
+
+if(name in l):
+    print("Your name in list")
+
+else:
+    print("your name is not in list")
