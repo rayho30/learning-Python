ba51f15cf77a323fb2f1c6bdf92f4195f058a717
Rayhan Islam Prome <rayhanspydet@gmail.com>
2026-06-20 22:52:38 +0600

    Add files via upload

---
+++ chapter 7/02_while_loop.py
@@ -0,0 +1,5 @@
+i = 1
+
+while(i<=10): #condition
+    print(i)
+    i += 1 #i=i+1
---
+++ chapter 7/03_list_while.py
@@ -0,0 +1,7 @@
+l = [1, "Orange", "Apple", "Banana", "Lemon"]
+
+i= 0
+
+while(i<len(l)):
+    print(l[i])
+    i += 1
