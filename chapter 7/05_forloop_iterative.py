bba83468cf29c972b5635ce2b425c2638ae8b09f
Rayhan Islam Prome <rayhanspydet@gmail.com>
2026-06-21 18:43:09 +0600

    Add files via upload

---
+++ chapter 7/05_forloop_iterative.py
@@ -0,0 +1,16 @@
+#for loop with list
+l = [1, 8, 7, 54, 30, 99, 12]
+
+for i in  l:
+    print(i)
+
+#for loop with tuple
+t = (1, 8, 67, 54, 30, 99, 12)
+
+for i in  t:
+    print(i)
+
+#for loop with string
+s = "Rayhan Islam"
+for i in s:
+    print(i)
