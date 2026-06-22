51e8f2cf47c736068196543c2737ec88caae702b
Rayhan Islam Prome <rayhanspydet@gmail.com>
2026-06-22 22:12:11 +0600

    Add files via upload

---
+++ chapter 7/07_break_and_continue.py
@@ -0,0 +1,10 @@
+for i in range (100):
+    if(i==50):
+        break #exit right now
+    print(i)
+    
+for i in range (100):
+    if(i==50):
+        continue #skip the value 
+    print(i)
+    
