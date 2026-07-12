f79dbe7c06e18668000e73764f4fc9622292f3ae
Rayhan Islam Prome <rayhanspydet@gmail.com>
2026-07-12 22:12:53 +0600

    Add files via upload

---
+++ chapter 9/2_file.py
@@ -0,0 +1,12 @@
+# Open the file in write mode
+f = open(r"F:\Pyrhon 21days\chapter 9\rayhan.txt", "w")
+
+# Write data to the file
+
+f.write("Hello Rayhan!\n")
+f.write("How are you?.")
+
+# Close the file
+f.close()
+
+print("Data written successfully.")
